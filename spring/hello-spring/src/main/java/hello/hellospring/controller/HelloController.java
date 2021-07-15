0bc52bf5b4891ef400d387eb5ba9a776db069cce
soyoonjeong <76814748+soyoonjeong@users.noreply.github.com>
2021-07-16 00:52:39 +0900

    mvc와 템플릿 엔진

--- spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -12,5 +13,14 @@ public class HelloController {
         model.addAttribute("data","hello!!");
         return "hello";
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        //http://localhost:8080/hello-mvc?name=soyoon 이런식으로 ?다음에 파라미터 전달
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+
+
 
 }
